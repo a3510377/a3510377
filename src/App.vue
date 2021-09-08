15ce18f5626d1e17127f6173db27695e571de24e
a3510377 <a102009102009@gmail.com>
2021-09-08 14:40:17 +0800

    add web

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Footer />
+  <router-view />
+  <Header />
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+export default {
+  components: {
+    Footer,
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
