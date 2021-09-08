4d797cf6315438c130a47008be7384aec03256c1
a3510377 <a102009102009@gmail.com>
2021-09-08 19:54:17 +0800

    set main file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     outputDir: "docs",
-    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/a3510377" : "/",
 };
